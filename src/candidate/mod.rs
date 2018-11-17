b8caf9e7be826f61297e6a590edf3863250ee3f6
Chris Patuzzo <chris@patuzzo.co.uk>
2018-11-17 15:17:04 +0000

    Implement Candidate#expand_one
    A candidate string can be expanded to build the
    next candidate strings with different symbols.
    
    We can short-circuit this step if the tail of the
    current or next candidate is not n-1 symbols long
    because this means new permutations definitely
    haven’t been added to the string.
    
    If these checks succeed, we construct the
    permutation, convert it to a decimal and check if
    we’ve seen it before in the BitSet.

--- src/candidate/mod.rs
+++ src/candidate/mod.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+use std::iter::once;
+
 struct Candidate {
     permutations_seen: BitSet,
     tail_of_string: Vec<usize>,
@@ -18,6 +20,75 @@ impl Candidate {
             tail_of_string: (1..n).collect(),
             wasted_symbols: 0,
         }
+    }
+
+    fn expand_one(&self, symbol: usize, n: usize) -> Self {
+        let tail_of_string = self.build_tail(symbol, n);
+
+        if Self::less_than_full(&self.tail_of_string, n) {
+            return self.candidate_with_wasted_symbol(tail_of_string);
+        }
+
+        if Self::less_than_full(&tail_of_string, n) {
+            return self.candidate_with_wasted_symbol(tail_of_string);
+        }
+
+        let id = self.permutation_id(symbol);
+
+        if self.permutations_seen.contains(id) {
+            return self.candidate_with_wasted_symbol(tail_of_string);
+        }
+
+        self.candidate_with_new_permutation(tail_of_string, id)
+    }
+
+    fn candidate_with_wasted_symbol(&self, tail_of_string: Vec<usize>) -> Self {
+        Candidate {
+            permutations_seen: self.permutations_seen.clone(),
+            tail_of_string: tail_of_string,
+            wasted_symbols: self.wasted_symbols + 1,
+        }
+    }
+
+    fn candidate_with_new_permutation(&self, tail_of_string: Vec<usize>, id: usize) -> Self {
+        let mut permutations_seen = self.permutations_seen.clone();
+        permutations_seen.insert(id);
+
+        let wasted_symbols = self.wasted_symbols;
+        Candidate { permutations_seen, tail_of_string, wasted_symbols }
+    }
+
+    fn less_than_full(tail_of_string: &Vec<usize>, n: usize) -> bool {
+        tail_of_string.len() < n - 1
+    }
+
+    // TODO: update Lehmer crate to accept a slice or iterator of usize
+    fn permutation_id(&self, symbol: usize) -> usize {
+        let permutation = self.tail_of_string
+            .iter()
+            .map(|&e| e as u8)
+            .chain(once(symbol as u8))
+            .collect();
+
+        Lehmer::from_permutation(permutation).to_decimal() as usize
+    }
+
+    fn build_tail(&self, symbol: usize, n: usize) -> Vec<usize> {
+        let head = &self.tail_of_string;
+
+        let index = match head.iter().position(|&e| e == symbol) {
+            Some(index) => index + 1,
+            None => match Self::less_than_full(head, n) {
+                true => 0,
+                false => 1,
+            }
+        };
+
+        Self::append(&head[index..], symbol)
+    }
+
+    fn append(slice: &[usize], symbol: usize) -> Vec<usize> {
+        slice.iter().map(|&e| e).chain(once(symbol)).collect()
     }
 }
 
--- src/candidate/test.rs
+++ src/candidate/test.rs
@@ -27,3 +27,118 @@ mod seed {
         assert_eq!(subject.wasted_symbols, 0);
     }
 }
+
+mod expand_one {
+    use super::*;
+
+    mod when_the_next_symbol_adds_a_permutation {
+        use super::*;
+
+        #[test]
+        fn it_has_seen_the_new_permutation() {
+            let subject = Subject::seed(5);
+            let candidate = subject.expand_one(0, 5);
+
+            let lehmer = Lehmer::from_permutation(vec![1, 2, 3, 4, 0]);
+            let decimal = lehmer.to_decimal() as usize;
+
+            assert_eq!(candidate.permutations_seen.contains(decimal), true);
+            assert_eq!(candidate.permutations_seen.len(), 2);
+        }
+
+        #[test]
+        fn it_builds_a_tail_from_the_end_of_the_permutation() {
+            let subject = Subject::seed(5);
+            let candidate = subject.expand_one(0, 5);
+
+            assert_eq!(candidate.tail_of_string, &[2, 3, 4, 0]);
+        }
+
+        #[test]
+        fn it_has_no_additional_wasted_symbols() {
+            let subject = Subject::seed(5);
+            let candidate = subject.expand_one(0, 5);
+
+            assert_eq!(candidate.wasted_symbols, 0);
+        }
+    }
+
+    mod when_the_next_symbol_does_not_add_a_permutation {
+        use super::*;
+
+        #[test]
+        fn it_has_not_seen_any_new_permutations() {
+            let subject = Subject::seed(5);
+            let candidate = subject.expand_one(3, 5);
+
+            assert_eq!(candidate.permutations_seen.len(), 1);
+        }
+
+        #[test]
+        fn it_builds_a_tail_after_the_repeated_symbol_in_the_previous_tail() {
+            let subject = Subject::seed(5);
+
+            let candidate = subject.expand_one(1, 5);
+            assert_eq!(candidate.tail_of_string, &[2, 3, 4, 1]);
+
+            let candidate = subject.expand_one(2, 5);
+            assert_eq!(candidate.tail_of_string, &[3, 4, 2]);
+
+            let candidate = subject.expand_one(3, 5);
+            assert_eq!(candidate.tail_of_string, &[4, 3]);
+
+            let candidate = subject.expand_one(4, 5);
+            assert_eq!(candidate.tail_of_string, &[4]);
+        }
+
+        #[test]
+        fn it_has_one_additional_wasted_symbol() {
+            let subject = Subject::seed(5);
+
+            let candidate = subject.expand_one(3, 5);
+            assert_eq!(candidate.wasted_symbols, 1);
+
+            let candidate = candidate.expand_one(3, 5);
+            assert_eq!(candidate.wasted_symbols, 2);
+        }
+    }
+
+    mod for_a_more_complicated_example {
+        use super::*;
+
+        #[test]
+        fn it() {
+            let n = 5;
+
+            let subject = Subject::seed(n);
+            assert_eq!(subject.permutations_seen.len(), 1);
+            assert_eq!(subject.tail_of_string, &[1, 2, 3, 4]);
+            assert_eq!(subject.wasted_symbols, 0);
+
+            let depth_1 = subject.expand_one(0, n);
+            assert_eq!(depth_1.permutations_seen.len(), 2);
+            assert_eq!(depth_1.tail_of_string, &[2, 3, 4, 0]);
+            assert_eq!(depth_1.wasted_symbols, 0);
+
+            let depth_2 = depth_1.expand_one(1, n);
+            assert_eq!(depth_2.permutations_seen.len(), 3);
+            assert_eq!(depth_2.tail_of_string, &[3, 4, 0, 1]);
+            assert_eq!(depth_2.wasted_symbols, 0);
+
+            let depth_3 = depth_2.expand_one(4, n);
+            assert_eq!(depth_3.permutations_seen.len(), 3);
+            assert_eq!(depth_3.tail_of_string, &[0, 1, 4]);
+            assert_eq!(depth_3.wasted_symbols, 1);
+
+            let depth_4 = depth_3.expand_one(3, n);
+            assert_eq!(depth_4.permutations_seen.len(), 3);
+            assert_eq!(depth_4.tail_of_string, &[0, 1, 4, 3]);
+            assert_eq!(depth_4.wasted_symbols, 2);
+
+            let depth_5 = depth_4.expand_one(0, n);
+            assert_eq!(depth_5.permutations_seen.len(), 4);
+            assert_eq!(depth_5.tail_of_string, &[1, 4, 3, 0]);
+            assert_eq!(depth_5.wasted_symbols, 2);
+        }
+    }
+}
